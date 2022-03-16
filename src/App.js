88679d13dbefbc22279f394db324630b7fcad412
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2022-03-16 19:29:27 +0530

    change structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './component/Header'
+import Page1 from './component/Page1'
+import Page2 from './component/Page2'
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Page1></Page1>
+      <Page2></Page2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../scss/Header.scss'
+function Header() {
+    return (
+        <div class="header">
+            <button></button>
+            <button></button>
+            <button></button>
+
+        </div>
+    )
+}
+
+export default Header
