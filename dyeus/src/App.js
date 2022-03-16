be7c47277796b71a3eb7d49ed4456724e8a0f281
Deepak Kumar <deepak.kumar@girnarsoft.co.in>
2022-03-16 17:46:21 +0530

    1st page

--- dyeus/src/App.js
+++ dyeus/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './component/Header'
+import Page1 from './component/Page1'
+import Page2 from './component/Page2'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Page1></Page1>
+      <Page2></Page2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ dyeus/src/component/Page2.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../scss/Page2.scss'
+function Page2() {
+    return (
+        <div class="page2">
+            <div class="para8">
+                <p id="para8_id">What makes us different?
+                </p>
+            </div>
+            <div class="para9">
+                <p id="para9_id">Reimagining and rewiring self-care
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Page2
